5d651be1a0aef8179ec5febfd96ddd97dc25ffa1
Luara <luaraap7@gmail.com>
2022-08-09 21:19:30 -0300

    exercícios prontos

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,19 @@
-//código a ser reescrito
+const nome = prompt('Qual o seu nome?')
+const idade = +prompt('Qual a sua idade?')
+
+//código  reescrito
 
 if(nome === "José"){
-	console.log("Oi, Zé!");
+    console.log("Oi, Zé!");
 } else {
-	console.log("Olá, "+nome);
+    console.log("Olá,"+ nome);
 }
 
 if(idade >= 18){
-	console.log("pode tirar carteira de motorista!");
+    console.log("pode tirar carteira de motorista!");
 } else {
-	console.log("Ainda não pode tirar carteira de motorista");
+    console.log("Ainda não pode tirar carteira de motorista");
 }
+
+// console.log(nome==='José'? 'Oi, Zé!': 'Olá, ' + nome)
+// console.log(idade>=18 ? 'Pode tirar carteira de motorista!': 'Ainda não pode tirar carteira de motorista')
